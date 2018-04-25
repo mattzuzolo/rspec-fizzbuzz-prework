f90fbbd031da2970a0e67e081159827015c29fab
Matt Zuzolo <mattzuzolo@outlook.com>
2018-04-25 16:35:22 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,19 +3,19 @@ def fizzbuzz(num)
   
     if num % 5 == 0 && num % 3 == 0
       
-      return "statement"
+      return "FizzBuzz"
     
     elsif num % 5 == 0 
     
-      return "statement"
+      return "Buzz"
       
     elsif num % 3 == 0
     
-      return "statement"
+      return "Fizz"
       
     else 
       
-      return "something else"
+      return nil
       
     end  
   

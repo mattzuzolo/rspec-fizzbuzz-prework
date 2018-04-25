c913274b03824e1d657ee62fe17dd1f939a8c1f7
Matt Zuzolo <mattzuzolo@outlook.com>
2018-04-25 16:34:51 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,23 @@
-def fizzbuzz
+def fizzbuzz(num)
   
   
-  
+    if num % 5 == 0 && num % 3 == 0
+      
+      return "statement"
+    
+    elsif num % 5 == 0 
+    
+      return "statement"
+      
+    elsif num % 3 == 0
+    
+      return "statement"
+      
+    else 
+      
+      return "something else"
+      
+    end  
   
   
   
